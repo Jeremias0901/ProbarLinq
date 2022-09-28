c5720b5fb1ef2b07cc803ad94129c46458e70815
Jeremias0901 <cuellojeremiasnatanael@gmail.com>
2022-09-28 01:41:22 -0300

    integracion de tres clases empresas, empleados, ControlEmpresasEmpleados + relacion entre las entidades. Una empresa tiene muchos empleados

--- ProbarLinq/Program.cs
+++ ProbarLinq/Program.cs
@@ -8,25 +8,133 @@ namespace ProbarLinq
     {
         static void Main(string[] args)
         {
-            int[] valoresNumericos = new int[10] { 2, 4, 6, 8, 53, 21, 6, 95, 1, 0 }; // origen de datos → lista
-            //List<int> numerosPares = new List<int>();
+            ControlEmpresasEmpleado tableEmpresasEmpleados = new ControlEmpresasEmpleado();
+            tableEmpresasEmpleados.getCeo();
 
-            //foreach (int numero in valoresNumericos)
-            //{
-            //    if(numero % 2 == 0)
-            //    {
-            //        numerosPares.Add(numero);
-            //    }
-            //}
+            tableEmpresasEmpleados.getEnterpriseName(new Empleado { Id = 5, Nombre = "Gerardo", Cargo = "FrontEnd", EmpresaId = 1, Salario = 1500.75});
+            tableEmpresasEmpleados.getEmployeesPildoras();
 
-            IEnumerable<int> numerosPares = from numero in valoresNumericos where numero % 2 == 0 select numero;
-            // en el lenguaje Linq podemos utilizzar expresiones lambda
 
-            Console.WriteLine("numero pares: ");
-            foreach (int numero in numerosPares)
+            // The Three Parts of a LINQ Query:
+            //  1. Data source.
+            int[] numbers = new int[10] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            // 2. Query creation.
+            // numQuery is an IEnumerable<int>
+            var numQuery =
+                from num in numbers
+                where (num % 2) == 0
+                select num;
+
+            List<int> numQuery2 =
+                (from num in numbers
+                 where (num % 2) == 0
+                 select num).ToList();
+
+            // or like this:
+            // numQuery3 is still an int[]
+
+            var numQuery3 =
+                (from num in numbers
+                 where (num % 2) == 0
+                 select num).ToArray();
+
+            // 3. Query execution.
+            foreach (int num in numQuery)
             {
-                Console.WriteLine(numero);
+                Console.Write("{0,1} ", num);
             }
+            Console.WriteLine();
+            foreach (int num in numQuery2)
+            {
+                Console.Write("{0,1} ", num);
+            }
+            Console.WriteLine();
+            foreach (int num in numQuery3)
+            {
+                Console.Write("{0,1} ", num);
+            }
+        }
+    }
+
+    class ControlEmpresasEmpleado
+    {
+        public ControlEmpresasEmpleado()
+        {
+            ListEmpresas =  new List<Empresa>();
+            ListEmpleados = new List<Empleado>();
+
+            ListEmpresas.Add(new Empresa { Id = 1, Nombre = "Google" });
+            ListEmpresas.Add(new Empresa { Id = 2, Nombre = "Youtube"});
+
+            ListEmpleados.Add(new Empleado { Id = 1, Nombre = "Jeremias", Cargo = "CEO", EmpresaId = 1, Salario = 100.50 });
+            ListEmpleados.Add(new Empleado { Id = 2, Nombre = "Natanael", Cargo = "CEO", EmpresaId = 2, Salario = 50.50 });
+            ListEmpleados.Add(new Empleado { Id = 3, Nombre = "Lucas", Cargo = "Co-CEO", EmpresaId = 2, Salario = 150.50 });
+            ListEmpleados.Add(new Empleado { Id = 4, Nombre = "Juan", Cargo = "Co-CEO", EmpresaId = 2, Salario = 50.50 });
+        }
+        public List<Empresa> ListEmpresas;
+        public List<Empleado> ListEmpleados;
+
+        public void getCeo()
+        {
+            // IEnumerable<Empleado> ceos = (from empleado in ListEmpleados where empleado.Cargo == "CEO" select empleado).FirstOrDefault();
+            IEnumerable<Empleado> ceos = from empleado in ListEmpleados where empleado.Cargo == "CEO" select empleado;
+
+            foreach (Empleado e in ceos)
+            {
+                // Console.WriteLine(e.Nombre);
+                e.getDatosEmpleado();
+            }
+        }
+
+        public void getEnterpriseName(Empleado e)
+        {
+            IEnumerable<Empresa> empresaResultante = from empresa in ListEmpresas where e.EmpresaId == empresa.Id select empresa;
+
+            Console.WriteLine();
+            e.getDatosEmpleado();
+            Console.WriteLine("pertenece a:");
+            empresaResultante.First().getDatosEmpresa();
+            Console.WriteLine();
+        }
+
+        public void getEmployeesPildoras()
+        {
+            IEnumerable<Empleado> empleadosResultantes = from empleado in ListEmpleados
+                                                           join empresa in ListEmpresas
+                                                             on empleado.EmpresaId equals empresa.Id
+                                                         where empresa.Nombre == "Youtube"
+                                                         select empleado;
+
+            foreach (Empleado empleado1 in empleadosResultantes)
+            {
+                empleado1.getDatosEmpleado();
+            }
+        }
+    }
+
+    class Empresa
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+
+        public void getDatosEmpresa()
+        {
+            Console.WriteLine("Empresa {0} con Id {1}", Nombre, Id);
+        }
+    }
+
+    class Empleado
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Cargo { get; set; }
+        public double Salario { get; set; }
+        public int  EmpresaId { get; set; }// clave foranea
+
+        public void getDatosEmpleado()
+        {
+            Console.WriteLine("Empleado {0} con Id {1}, Cargo {2}, Salario {3}, En la Empresa {4}", Nombre, Id, Cargo, Salario, EmpresaId);
         }
     }
 }
