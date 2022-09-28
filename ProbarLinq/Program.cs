59458325d41868f5a57b307e7d9d996fc39a7bf3
Jeremias0901 <cuellojeremiasnatanael@gmail.com>
2022-09-27 23:54:50 -0300

    Programa con Linq

--- ProbarLinq/Program.cs
+++ ProbarLinq/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ProbarLinq
 {
@@ -7,19 +8,21 @@ namespace ProbarLinq
     {
         static void Main(string[] args)
         {
-            int[] valoresNumericos = new int[10] { 2, 4, 6, 8, 53, 21, 6, 95, 1, 0 };
-            List<int> numerosPares = new List<int>();
+            int[] valoresNumericos = new int[10] { 2, 4, 6, 8, 53, 21, 6, 95, 1, 0 }; // origen de datos → lista
+            //List<int> numerosPares = new List<int>();
 
-            foreach (int numero in valoresNumericos)
-            {
-                if(numero % 2 == 0)
-                {
-                    numerosPares.Add(numero);
-                }
-            }
+            //foreach (int numero in valoresNumericos)
+            //{
+            //    if(numero % 2 == 0)
+            //    {
+            //        numerosPares.Add(numero);
+            //    }
+            //}
+
+            IEnumerable<int> numerosPares = from numero in valoresNumericos where numero % 2 == 0 select numero;
+            // en el lenguaje Linq podemos utilizzar expresiones lambda
 
             Console.WriteLine("numero pares: ");
-
             foreach (int numero in numerosPares)
             {
                 Console.WriteLine(numero);
